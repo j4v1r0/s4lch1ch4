853fa29e7768147463f1f6b4eb1587d3a9cdabb4
j4v1r0 <73962897+j4v1r0@users.noreply.github.com>
2021-07-12 22:18:34 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect("mysql.test-mtc-app-bbdd-pv-2.svc", "wpuser", "redhat123", "phpdatabase") or die("could not connect" . mysqli_error($conn) ) ;
+$conn = mysqli_connect("mysql.test-mtc-app-bbdd-pv-2.svc.cluster.local", "wpuser", "redhat123", "phpdatabase") or die("could not connect" . mysqli_error($conn) ) ;
 ?>
